50dd202ba75b7fe380983767e27548eedf39222a
hasan-m.shehata <hasan.mh.sh@gmail.com>
2020-10-29 00:45:25 +0200

    uncomment the following line to initialize the datbase

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,9 +3,11 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_filename = "database.db"
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+# database_filename = "database.db"
+# project_dir = os.path.dirname(os.path.abspath(__file__))
+# database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_name = "cafee"
+database_path = "postgres://{}:{}@{}/{}".format('postgres', 'postgres','localhost:5432', database_name)
 
 db = SQLAlchemy()
 
