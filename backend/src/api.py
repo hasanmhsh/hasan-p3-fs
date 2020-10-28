477e1637a7784ca9358bc8838ba9726d80b87e06
hasan-m.shehata <hasan.mh.sh@gmail.com>
2020-10-29 01:00:35 +0200

    GET /drinks-detail         it should require the get:drinks-detail permission         it should contain the drink.long() data representation     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks         or appropriate status code indicating reason for failure

--- backend/src/api.py
+++ backend/src/api.py
@@ -3,9 +3,140 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
+
+# Configuration
+# AUTH0 ACCOUNT
+AUTH0_DOMAIN = 'dev-tv8ilm54.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'cafee'
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+## Auth Header
+def verify_decode_jwt(token):
+    # GET THE PUBLIC KEY FROM AUTH0
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    
+    # GET THE DATA IN THE HEADER
+    unverified_header = jwt.get_unverified_header(token)
+    
+    # CHOOSE OUR KEY
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    
+    # Finally, verify!!!
+    if rsa_key:
+        try:
+            # USE THE KEY TO VALIDATE THE JWT
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            # print('successful')
+            # print(payload)
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
+def get_token_auth_header():
+    #check if http request has 'Authorization' header
+    if 'Authorization' not in request.headers:
+        abort(401)
+
+    # auth header example -> 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZ2FiZSIsInNjaG9vbCI6InVkYWNpdHkiLCJyb2xlIjoiaW5zdHJ1Y3RvciJ9.T9hKh61bM-lFqvntAWrqPLWxAH-Ig0usQVwiVcJ1g5g'
+    auth_header = request.headers['Authorization']
+    # we don't wand 'Bearer' word we just want the jwt
+    header_parts = auth_header.split(' ')
+
+    # check if the authentication type is 'Bearer'
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != 'bearer':
+        abort(401)
+
+    return header_parts[1]
+
+def requires_auth(permissions=[]):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            jwt = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(jwt)
+            except:
+                abort(403)
+            if permissions != None:
+                if len(permissions) > 0:
+                    for permission in permissions:
+                        check_permissions(permission, payload)
+                else:
+                    check_permissions('', payload)
+            else:
+                check_permissions('', payload)
+            return f(payload,*args, **kwargs)
+        return wrapper
+    return requires_auth_decorator
+
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+                        raise AuthError({
+                            'code': 'invalid_claims',
+                            'description': 'Permissions not included in JWT.'
+                        }, 400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
+    return True
 
 app = Flask(__name__)
 setup_db(app)
@@ -20,7 +151,7 @@ CORS(app)
 
 ## ROUTES
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
@@ -39,7 +170,7 @@ def get_abstract_drinks():
         abort(404)
 
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
@@ -47,6 +178,17 @@ def get_abstract_drinks():
         or appropriate status code indicating reason for failure
 '''
 
+@app.route('/drinks-detail')
+@requires_auth(['get:drinks-detail'])
+def get_detailed_drink():
+        try:
+        drinks = Drink.query.order_by(Drink.id)
+        return jsonify({
+            'success': True,
+            'drinks': [drink.long() for drink in drinks]
+        })
+    except:
+        abort(404)
 
 '''
 @TODO implement endpoint
